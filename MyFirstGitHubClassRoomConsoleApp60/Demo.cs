cc3ab682b3b181597ae19bdbbf18c7f9ef84f542
smillingleeenfield <Magnushdguldager@gmail.com>
2025-08-27 10:55:09 +0200

    this sucks

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+        private int Value = 0;
+        public int MyProperty { get; set; }
+        public int MyValue { get { return Value; } set { Value = value; } }
+    }
+}
